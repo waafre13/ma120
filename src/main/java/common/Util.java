aa3bb9cb811ca379d8fb38707c6c1466a1b3e41e
Fredric <waafre13@student.westerdals.no>
2016-10-17 16:21:23 +0200

    Initial Commit

---
+++ src/main/java/common/Util.java
@@ -0,0 +1,36 @@
+package common;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Created by Fredric on 17/10/2016.
+ */
+public class Util {
+
+    // For multiple attributes at once
+    public static String[] getAttrContent(String[] attrNames, String text){
+
+        String[] contentList = new String[attrNames.length];
+        int i = 0;
+        for (String attrName: attrNames) {
+            contentList[i] = getAttrContent(attrName, text);
+            i++;
+        }
+
+        return contentList;
+    }
+
+    // For single attribute
+    public static String getAttrContent(String attrName, String text){
+
+        String content;
+        String regex = "("+attrName+"=\")([^\"]+)";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher match = pattern.matcher(text);
+        content = match.find() ? match.group(2) : "";
+
+        return content;
+    }
+}
+
