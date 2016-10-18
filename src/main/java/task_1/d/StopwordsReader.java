5a867f5ef24fc1df50edcc344af0552a62bc7c97
Fredric <waafre13@student.westerdals.no>
2016-10-18 13:20:09 +0200

    Finished stopwords task

--- src/main/java/task_1/d/StopwordsReader.java
+++ src/main/java/task_1/d/StopwordsReader.java
@@ -1,7 +1,45 @@
 package task_1.d;
 
-/**
- * Created by Fredric on 18/10/2016.
- */
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class StopwordsReader {
+
+    private List<String> stopwords = new ArrayList<String>();
+
+    public StopwordsReader(final String stopwordspath){
+        readFile(stopwordspath);
+    }
+
+    private void readFile(final String stopwordspath){
+        BufferedReader reader = null;
+
+        try {
+            File file = new File(stopwordspath);
+            reader = new BufferedReader(new FileReader(file));
+
+            String line;
+            while ((line = reader.readLine()) != null) {
+                stopwords.add(line.toLowerCase());
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                reader.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public List<String> getStopwords(){
+        return stopwords;
+    }
 }
