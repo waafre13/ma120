70b1cd091ca2ca342531ff8991c7a8e03f52a8d4
Fredric <waafre13@student.westerdals.no>
2016-10-19 15:44:10 +0200

    Added Util.isInteger method

--- src/main/java/common/Util.java
+++ src/main/java/common/Util.java
@@ -24,7 +24,6 @@ public class Util {
     // Returns attribute content for a single attribute
     public static String getAttrContent(String attrName, String text){
 
-        String content;
         String regex = "(?:"+attrName+"=\")([^\"]+)";
         Pattern pattern = Pattern.compile(regex);
         Matcher match = pattern.matcher(text);
@@ -37,5 +36,92 @@ public class Util {
     public static String removeHTMLTags(String string){
         return string.replaceAll("(?i)&(?:[a-z\\d]+|#\\d+|#x[a-f\\d]+);"," ");
     }
+
+    public static boolean isInteger(String str) {
+        if (str == null) {
+            return false;
+        }
+        int length = str.length();
+        if (length == 0) {
+            return false;
+        }
+        int i = 0;
+        if (str.charAt(0) == '-') {
+            if (length == 1) {
+                return false;
+            }
+            i = 1;
+        }
+        for (; i < length; i++) {
+            char c = str.charAt(i);
+            if (c < '0' || c > '9') {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /*
+    public class KeyValueArray{
+
+        private int[] keyArray = new int[];
+        private String[] valArray = new String[];
+
+        public KeyValueArray()
+        {
+        }
+
+        public KeyValueArray(int[] key, String[] val){
+            keyArray = key;
+            valArray = val;
+        }
+
+        public int[] getKeyArray(){
+            return keyArray;
+        }
+
+        public String[] getValArray(){
+            return valArray;
+        }
+
+        public void sortArraysByKey(){
+
+            // Sort by reputation, descending
+            for (int i = 0; i < keyArray.length; i++){
+                for (int j = 0; j < keyArray.length; j++){
+                    if(keyArray[i] > keyArray[j]){
+
+                        int tempKey = keyArray[j];
+                        keyArray[j] = keyArray[i];
+                        keyArray[i] = tempKey;
+
+                        String tempVal = valArray[j];
+                        valArray[j] = valArray[i];
+                        valArray[i] = tempVal;
+                    }
+                }
+            }
+        }
+
+        public void sortArraysByValue(){
+
+            // Sort by reputation, descending
+            for (int i = 0; i < valArray.length; i++){
+                for (int j = 0; j < valArray.length; j++){
+                    if(valArray[i] > valArray[j]){
+
+                        int tempKey = valArray[j];
+                        valArray[j] = valArray[i];
+                        valArray[i] = tempKey;
+
+                        String tempVal = keyArray[j];
+                        keyArray[j] = keyArray[i];
+                        keyArray[i] = tempVal;
+                    }
+                }
+            }
+
+        }
+    }*/
 }
 
