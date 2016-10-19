23b5cfc05a117f0f409f031cf2871a2db5df269c
Fredric <waafre13@student.westerdals.no>
2016-10-19 12:51:01 +0200

    Added task_2d

---
+++ src/main/java/task_2/d/TopQuestionsMapper.java
@@ -0,0 +1,22 @@
+package task_2.d;
+
+import common.Util;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import java.io.IOException;
+
+class TopDBAsMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
+
+    @Override
+    protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+        String text = value.toString();
+
+        int rep = Integer.parseInt(Util.getAttrContent("Reputation", text));
+        String name = Util.getAttrContent("DisplayName", text);
+
+        context.write(new Text(name), new IntWritable(rep));
+    }
+}
