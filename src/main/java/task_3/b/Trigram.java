0f2e0990c1b1255d437b9c4b7b5c030e687c40b0
Fredric <waafre13@student.westerdals.no>
2016-10-19 17:54:51 +0200

    Added task_3[a,b]

---
+++ src/main/java/task_3/b/Trigram.java
@@ -0,0 +1,54 @@
+package task_3.a;
+
+import common.XmlInputFormat;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+/*
+3.a) Bigram. A pair of adjacent words is called a bigram. For example, \big
+data" or \fast car" are examples of bigrams. Find the most common
+bigram in the titles of the questions.
+
+Run command:
+
+*/
+
+public class Bigram {
+
+    public static void main(String[] args) throws Exception {
+
+        // Check arguments
+        if(args.length != 2){
+            System.out.println("Wrong number of arguments. Use: <class> <input_path> <output_path>");
+            return;
+        }
+
+        // Handle log4j exception errors
+        org.apache.log4j.BasicConfigurator.configure();
+
+        Job job = Job.getInstance(new Configuration());
+
+        job.setJarByClass(Bigram.class);
+        job.setInputFormatClass(XmlInputFormat.class);
+
+        job.setMapperClass(BigramMapper.class);
+        job.setReducerClass(BigramReducer.class);
+
+        job.setMapOutputKeyClass(Text.class);
+        job.setMapOutputValueClass(IntWritable.class);
+
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(LongWritable.class);
+
+        FileInputFormat.setInputPaths(job, args[0]);
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+        job.waitForCompletion(true);
+    }
+}
