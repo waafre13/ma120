933eca8f7ffcea1e748700e85988921b75d97f5b
Fredric <waafre13@student.westerdals.no>
2016-10-19 16:11:58 +0200

    Added task_2f

---
+++ src/main/java/task_2/f/AverageAnswers.java
@@ -0,0 +1,56 @@
+package task_2.e;
+
+import common.XmlInputFormat;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+/*
+2.d) Top questions. Write a Hadoop MapReduce job that outputs top 10
+questions in terms of their reputation.
+
+Since questions does not have a reputation attribute, we assume that score is what was meant.
+Otherwise it could be reputation of the creator of the question.
+
+Run command:
+
+*/
+
+public class FavouriteQuestions {
+
+    public static void main(String[] args) throws Exception {
+
+        // Check arguments
+        if(args.length != 2){
+            System.out.println("Wrong number of arguments. Use: <class> <input_path> <output_path>");
+            return;
+        }
+
+        // Handle log4j exception errors
+        org.apache.log4j.BasicConfigurator.configure();
+
+        Job job = Job.getInstance(new Configuration());
+
+        job.setJarByClass(FavouriteQuestions.class);
+        job.setInputFormatClass(XmlInputFormat.class);
+
+        job.setMapperClass(FavouriteQuestionsMapper.class);
+        job.setReducerClass(FavouriteQuestionsReducer.class);
+
+        job.setMapOutputKeyClass(Text.class);
+        job.setMapOutputValueClass(IntWritable.class);
+
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(LongWritable.class);
+
+        FileInputFormat.setInputPaths(job, args[0]);
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+        job.waitForCompletion(true);
+    }
+}
---
+++ src/main/java/task_2/f/AverageAnswersMapper.java
@@ -0,0 +1,30 @@
+package task_2.e;
+
+import common.Util;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import java.io.IOException;
+
+class FavouriteQuestionsMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
+
+    @Override
+    protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+        String text = value.toString();
+
+        String postTypeId = Util.getAttrContent("PostTypeId", text);
+
+        // Filter questions
+        if(postTypeId.equals("1")){
+            String title = Util.getAttrContent("Title", text);
+            String count = Util.getAttrContent("FavoriteCount", text);
+
+            // Check if count has a valid value (can be converted to an integer)
+            if(Util.isInteger(count)){
+                context.write(new Text(title), new IntWritable(Integer.parseInt(count)));
+            }
+        }
+    }
+}
